94666210cf6cd7d2825ea6b055fddaa9383add76
an0304073@naver.com <an0304073@naver.com>
2020-11-24 02:54:55 +0900

    [ADD] Profile

--- src/components/MyInfo/MyInfo.js
+++ src/components/MyInfo/MyInfo.js
@@ -4,8 +4,20 @@ import './MyInfo.scss';
 const MyInfo = () => {
     return (
         <div className="myPage">
-            <div className="myPage-background">
+            <div className="myPage-background"/>
+            <div className="myPage-book-wrapper">
+                <div className="myPage-profile-search--wrapper">
+                    <div className="myPage-profile">
+                        <div className="myPage-profile-circle"/>
+                        <div className="myPage-profile-number">2학년 1반 11번 신서림</div>
+                    </div>
+                    <div className="myPage-search">
 
+                    </div>
+                </div>
+                <div className="myPage-loan">
+
+                </div>
             </div>
         </div>
     )
