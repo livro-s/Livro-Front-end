2bfbe8f8cf99b228b6dd860cf8be23438d4a4c6e
yiyb0603 <yiyb0603@gmail.com>
2020-11-24 00:35:08 +0900

    [ADD] SearchBook page styling

--- src/components/SearchBook/SearchBook.js
+++ src/components/SearchBook/SearchBook.js
@@ -1,10 +1,33 @@
 import React from "react";
 import "./SearchBook.scss";
+import { GoSearch } from 'react-icons/go';
+import BookIcon from 'assets/icons/BookIcon.png';
+import { Palette } from "styles/Palette/Palette";
 
 const SearchBook = () => {
+  const { main } = Palette;
+
   return (
     <div className="SearchBook">
-      <div></div>
+      <div className="SearchBook-Background"></div>
+      <div className="SearchBook-TopWrapper">
+        <div className="SearchBook-TopWrapper-InputWrapper">
+          <div className="SearchBook-TopWrapper-InputWrapper-InputBox">
+            <img className="SearchBook-TopWrapper-InputWrapper-InputBox-Icon" src ={BookIcon} alt ="bookicon" />
+            <input type ="text" placeholder="찾고싶은 도서를 검색해보세요" />
+          </div>
+
+          <GoSearch className="SearchBook-TopWrapper-InputWrapper-InputIcon" />
+        </div>
+
+        <button className="SearchBook-TopWrapper-Button">검색</button>
+      </div>
+
+      <div className="SearchBook-UnderBorder"></div>
+      <div className="SearchBook-SearchList">
+        <div className="SearchBook-SearchList-NoSearch">LIVRO<span style ={{ color: main }}>'S</span></div>
+        <div className="SearchBook-SearchList-SubTitle">어디서든 편하게 도서관 서비스를 이용해보세요</div>
+      </div>
     </div>
   );
 };
