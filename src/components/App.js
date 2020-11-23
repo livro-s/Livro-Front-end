45bd35de0e56425c36c780a5cfc6056941eff031
an0304073@naver.com <an0304073@naver.com>
2020-11-23 21:17:48 +0900

    [ADD] Page initial Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
 const App = () => {
   return (
     <>
-      <div>React Mobx PCC default Setting JS</div>
+      <Route exact path="/" component={() => <div>메인 페이지입니다.</div>} />
+      <Route path="/notice" component={() => <div>공지사항 페이지입니다.</div>} />
+      <Route path="/myPage" component={() => <div>마이페이지입니다.</div>} />
+      <Route path="/search" component={() => <div>도서 검색 페이지입니다</div>} />
     </>
   );
 };
