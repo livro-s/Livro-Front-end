f2f57b7b5f18f8943165abaab703bde628f9e277
an0304073@naver.com <an0304073@naver.com>
2020-11-24 02:08:52 +0900

    [ADD] MyPage & MyInfo

---
+++ src/components/MyInfo/MyInfo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MyInfo.scss';
+
+const MyInfo = () => {
+    return (
+        <div className="myPage">
+            <div className="myPage-background">
+
+            </div>
+        </div>
+    )
+}
+
+export default MyInfo;
