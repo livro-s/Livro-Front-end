591afc9aa6b651e55c2ba9c4f06102e6e9fdd02a
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-24 01:41:19 +0900

    [ADD] NoticeCard

---
+++ src/pages/NoticePage.js
@@ -0,0 +1,12 @@
+import PageTemplate from 'components/Common/PageTemplate';
+import React from 'react';
+import Notice from 'components/Notice';
+const NoticePage = () => {
+  return (
+    <PageTemplate>
+      <Notice />
+    </PageTemplate>
+  );
+};
+
+export default NoticePage;
