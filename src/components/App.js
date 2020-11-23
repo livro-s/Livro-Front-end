064811ae841931eb9ae6e8103931d53a7e27af95
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-23 23:32:30 +0900

    [ADD] Route Auth path

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import * as Pages from 'pages';
+import AuthContainer from 'container/AuthContainer/AuthContainer';
 
 const App = () => {
   const { Main, Search } = Pages;
 
   return (
     <Switch>
-      <Route exact path ="/" component={Main} />
-      <Route exact path ="/search" component={Search} />
-      <Route path="/notice" component={() => <div>공지사항 페이지입니다.</div>} />
+      <Route exact path="/" component={Main} />
+      <Route exact path="/search" component={Search} />
+      <Route exact path="/auth" component={<AuthContainer />} />
+      <Route
+        path="/notice"
+        component={() => <div>공지사항 페이지입니다.</div>}
+      />
       <Route path="/myPage" component={() => <div>마이페이지입니다.</div>} />
     </Switch>
   );
