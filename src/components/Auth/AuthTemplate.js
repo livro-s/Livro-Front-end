b3e62e0ab08d84e936fa06e0992ca5c1044b5a5c
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-24 13:19:11 +0900

    [ADD] Toast, Login,Register Container, Store, Validation

--- src/components/Auth/AuthTemplate.js
+++ src/components/Auth/AuthTemplate.js
@@ -3,15 +3,51 @@ import './AuthTemplate.scss';
 import Login from './Login/Login';
 import Register from './Register/Register';
 
-const AuthTemplate = ({ isRegister, handleIsRegister, handleIsLogin }) => {
+const AuthTemplate = ({
+  isRegister,
+  handleIsRegister,
+  handleIsLogin,
+  requestHandleLogin,
+  userId,
+  setUserId,
+  password,
+  setPassword,
+  name,
+  setName,
+  school,
+  setSchool,
+  studentNo,
+  setStudentNo,
+  requestHandleRegister,
+}) => {
   return (
     <div className="AuthTemplate">
       <div className="AuthTemplate-AuthBox">
         <div className="AuthTemplate-AuthBox-InnerBox">
           {isRegister ? (
-            <Register handleIsLogin={handleIsLogin} />
+            <Register
+              handleIsLogin={handleIsLogin}
+              userId={userId}
+              setUserId={setUserId}
+              password={password}
+              setPassword={setPassword}
+              name={name}
+              setName={setName}
+              school={school}
+              setSchool={setSchool}
+              studentNo={studentNo}
+              setStudentNo={setStudentNo}
+              requestHandleRegister={requestHandleRegister}
+            />
           ) : (
-            <Login handleIsRegister={handleIsRegister} />
+            <Login
+              handleIsRegister={handleIsRegister}
+              requestHandleLogin={requestHandleLogin}
+              userId={userId}
+              setUserId={setUserId}
+              password={password}
+              setPassword={setPassword}
+            />
           )}
         </div>
       </div>
--- src/container/AuthContainer/AuthContainer.js
+++ src/container/AuthContainer/AuthContainer.js
@@ -1,10 +1,78 @@
 import React, { useCallback, useState } from 'react';
 import useStores from 'lib/hooks/useStores';
 import AuthTemplate from 'components/Auth/AuthTemplate';
-
-const AuthContainer = () => {
-  //   const { store } = useStores();
+import {
+  ValidationRegister,
+  ValidationLogin,
+  ValidationEmail,
+} from 'validation/Auth/AuthValidation';
+import { SuccessToast, ErrorToast } from 'lib/Toast';
+import { withRouter } from 'react-router-dom';
+const AuthContainer = ({ history }) => {
+  const { store } = useStores();
+  const { handleLogin, handleRegister } = store.AuthStore;
   const [isRegister, setIsRegister] = useState(false);
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [school, setSchool] = useState('');
+  const [studentNo, setStudentNo] = useState('');
+
+  const requestHandleLogin = useCallback(async () => {
+    const data = {
+      userId,
+      password,
+    };
+    if (!ValidationLogin(data)) {
+      return;
+    }
+    await handleLogin(data)
+      .then((res) => {
+        const { status } = res;
+        if (status === 200) {
+          SuccessToast('로그인 성공');
+          history.push('/');
+        }
+      })
+      .catch((err) => {
+        const { status } = err.response;
+        if (status === 400) {
+          ErrorToast('아이디나 비밀번호 실패');
+        }
+      });
+  }, [handleLogin, history, password, userId]);
+
+  const requestHandleRegister = useCallback(async () => {
+    const data = {
+      userId,
+      password,
+      name,
+      school,
+      studentNo,
+    };
+    if (!ValidationRegister(data)) {
+      return;
+    }
+    if (!ValidationEmail(data)) {
+      return;
+    }
+    await handleRegister(data)
+      .then((res) => {
+        const { status } = res;
+        if (status === 201) {
+          if (status === 201) {
+            SuccessToast('회원가입 성공');
+            setIsRegister(false);
+          }
+        }
+      })
+      .catch((err) => {
+        const { status } = err.response;
+        if (status === 409) {
+          ErrorToast('이메일 중복');
+        }
+      });
+  }, [handleRegister, name, password, school, studentNo, userId]);
 
   const handleIsRegister = useCallback(() => {
     setIsRegister(true);
@@ -16,6 +84,18 @@ const AuthContainer = () => {
 
   return (
     <AuthTemplate
+      userId={userId}
+      setUserId={setUserId}
+      password={password}
+      setPassword={setPassword}
+      requestHandleLogin={requestHandleLogin}
+      name={name}
+      setName={setName}
+      school={school}
+      setSchool={setSchool}
+      studentNo={studentNo}
+      setStudentNo={setStudentNo}
+      requestHandleRegister={requestHandleRegister}
       isRegister={isRegister}
       handleIsRegister={handleIsRegister}
       handleIsLogin={handleIsLogin}
@@ -23,4 +103,4 @@ const AuthContainer = () => {
   );
 };
 
-export default AuthContainer;
+export default withRouter(AuthContainer);
