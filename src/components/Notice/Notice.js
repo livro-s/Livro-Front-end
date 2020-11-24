474c6ba165612538b23a29b73e7eb9314c737f24
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-24 09:55:49 +0900

    [ADD] Noitce, NoticeInfo

--- src/components/Notice/Notice.js
+++ src/components/Notice/Notice.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Notice.scss';
 import NoticeCard from './NoticeCard/NoticeCard';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 const Notie = () => {
   return (
     <>
@@ -10,6 +11,15 @@ const Notie = () => {
 
         <NoticeCard />
         <NoticeCard />
+      </div>
+      <div className="NoticePageNationWrap">
+        <div className="NoticePageNationWrap-PrevButton">
+          <FiChevronLeft />
+        </div>
+        <div className="NoticePageNationWrap-CurrentPage">1</div>
+        <div className="NoticePageNationWrap-NextButton">
+          <FiChevronRight />
+        </div>
       </div>
     </>
   );
