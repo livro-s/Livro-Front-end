d1ad5dd2682010d33ff013027ea8b1f7bedee05c
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-24 10:53:51 +0900

    [ADD] MainNotice

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import * as Pages from 'pages';
 import AuthContainer from 'container/AuthContainer/AuthContainer';
+import NoticePage from 'pages/NoticePage';
 
 const App = () => {
   const { Main, Search, MyPage } = Pages;
@@ -11,11 +12,9 @@ const App = () => {
       <Route exact path="/" component={Main} />
       <Route exact path="/search" component={Search} />
       <Route exact path="/auth" component={AuthContainer} />
-      <Route
-        path="/notice"
-        component={() => <div>공지사항 페이지입니다.</div>}
-      />
+      <Route path="/notice" component={Pages.Notice} />
       <Route path="/myPage" component={MyPage} />
+      <Route path="/noticeInfo/:idx" component={Pages.NoticeInfo} />
     </Switch>
   );
 };
